5d610878574c43789c8885383f9a4d2a4ba87de3
MchamberC <65751373+MchamberC@users.noreply.github.com>
2020-07-23 20:22:20 -0400

    Add files via upload

---
+++ Codigo.py
@@ -0,0 +1,38 @@
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import smtplib, ssl
+
+#correo
+username = ('')
+#password correo
+password = ('')
+
+#correo de remitente
+recipient = ('')
+
+#asunto
+affair = ("")
+
+message = MIMEMultipart("alternative")
+message ["subject"] = affair
+message ["From"] = username
+message ["To"] = recipient
+
+#codigo html5
+html = f""" 
+ 
+"""
+
+html_part = MIMEText(html, "html")
+message.attach(html_part)
+
+conection = ssl.create_default_context()
+
+
+#servidor puerto
+with smtplib.SMTP_SSL("", 595, context=conection) as server:
+    server.login(username, password)
+    print("correct login")  
+    server.sendmail(username, recipient, message.as_bytes())
+    print("message sent")
+
